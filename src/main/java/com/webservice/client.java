87a73fd5cb18cf11ff2819dc856dbf9dc69707db
arorah1 <arorah1@emc.com>
2013-10-20 22:24:06 +0530

    First commit

---
+++ src/main/java/com/webservice/client.java
@@ -0,0 +1,20 @@
+package com.webservice;
+
+import localhost.FirstWebservice.Hello_jws.Hello;
+import localhost.FirstWebservice.Hello_jws.HelloService;
+import localhost.FirstWebservice.Hello_jws.HelloServiceLocator;
+
+public class client {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		HelloServiceLocator hsl = new HelloServiceLocator();
+		Hello hello = hsl.getHello();
+		String str = hello.hi("Sonia");
+		int a = 10;
+		int b = 15;
+		int sum = hello.sum(a, b);
+		System.out.println("Sum of a + b = " + sum);
+	}
+
+}
---
+++ src/main/java/localhost/FirstWebservice/Hello_jws/Hello.java
@@ -0,0 +1,13 @@
+/**
+ * Hello.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package localhost.FirstWebservice.Hello_jws;
+
+public interface Hello extends java.rmi.Remote {
+    public java.lang.String hi(java.lang.String str) throws java.rmi.RemoteException;
+    public int sum(int a, int b) throws java.rmi.RemoteException;
+}
